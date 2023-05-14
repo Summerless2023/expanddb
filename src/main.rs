b0973b771d5bc0eed8276144706feec17d9e3c01
Summerless <yfzhang_519@stu.xidian.edu.cn>
2023-05-14 11:53:44 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ pub mod file;
 pub use config::config::*;
 pub use file::handler;
 use log::warn;
+use log4rs;
 
 #[macro_use]
 extern crate lazy_static;
 
 fn main() {
+    log4rs::init_file("src/config/log4rs.yaml", Default::default()).unwrap();
     init_config_map();
     print!("{:?}", *MAP);
     warn!("-----");
